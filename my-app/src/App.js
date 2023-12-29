4c10ef90ba805274c4af4ffa77d674c5eb55eeee
福原優太 <fuyu0853@icloud.com>
2023-12-30 01:13:42 +0900

    ラーメン屋検索機能

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); // レビューデータ用の状態
+  const [shops, setShops] = useState([]); // ラーメン店のデータ用の状態
 
+  const [searchQuery, setSearchQuery] = useState(''); // 検索クエリ用の状態
+
+  // レビューデータの取得
   useEffect(() => {
     axios.get('http://localhost:3000/reviews')
       .then((response) => {
@@ -14,9 +18,33 @@ function App() {
       });
   }, []);
 
+  /*
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/hotpepper/ramen_shops')
+      .then(response => {
+        console.log(response.data); // ここでレスポンスデータをログ出力
+        if (response.data && Array.isArray(response.data)) {
+          setShops(response.data);
+        }
+      })
+      .catch(error => console.error('APIリクエストエラー:', error));
+  }, []);
+  */
+
+  const handleSearch = () => {
+    axios.get(`http://localhost:3000/api/hotpepper/ramen_shops?query=${encodeURIComponent(searchQuery)}`)
+      .then(response => {
+        if (response.data && Array.isArray(response.data)) {
+          setShops(response.data); // 検索結果をshopsにセット
+        }
+      })
+      .catch(error => console.error('APIリクエストエラー:', error));
+  };
+  
+
   return (
     <div>
-      <h1>ラーメンレビュー一覧</h1>
+      <h1>レビュー一覧</h1>
       <ul>
         {data.map((review) => (
           <li key={review._id}>
@@ -27,6 +55,21 @@ function App() {
           </li>
         ))}
       </ul>
+      <h2>ラーメン店一覧</h2>
+      <ul>
+        {Array.isArray(shops) && shops.map((shop) => (
+          <li key={shop.id}>{shop.name}</li> // ラーメン店名を表示
+    ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="ラーメン店を検索"
+        />
+        <button onClick={handleSearch}>検索</button>
+      </div>
     </div>
   );
 }
