83a743d2ab48a652bc777780e8597ff744338c6e
福原優太 <fuyu0853@icloud.com>
2024-01-22 01:58:57 +0900

    レビューリストの一覧表示変更

--- my-app/src/ReviewList.js
+++ my-app/src/ReviewList.js
@@ -1,28 +1,32 @@
 // 選択されたラーメン店のレビュー一覧
 import React from 'react';
 
+import './styles/ReviewList.css';
+
 const ReviewList = ({ reviews }) => {
     const renderReviews = () => {
         if (reviews.length === 0) {
-        return <p>レビューはありません。</p>;
+            return <p>レビューはありません。</p>;
         }
 
         return reviews.map((review) => (
-        <li key={review._id}>
-            <p>投稿者: {review.write_name}</p>
-            <p>評価点: {review.review_points}</p>
-            <p>レビュー: {review.review}</p>
-            <p>投稿日: {new Date(review.created_at).toLocaleDateString()}</p>
-        </li>
+            <li key={review._id}>
+                <p>投稿者: {review.write_name}</p>
+                <p>評価点: {review.review_points}</p>
+                <p>レビュー: {review.review}</p>
+                <p>投稿日: {new Date(review.created_at).toLocaleDateString()}</p>
+            </li>
         ));
     };
 
     return (
-        <div>
-        <h2>レビュー一覧</h2>
-        <ul>
-            {renderReviews()}
-        </ul>
+        <div className="reviewListContainer">
+            <h2>レビュー一覧</h2>
+            <div className="reviewList">
+                <ul>
+                    {renderReviews()}
+                </ul>
+            </div>
         </div>
     );
 };
